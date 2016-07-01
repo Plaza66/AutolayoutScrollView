d944e46ab746a7b5f1d28f2f0354e44bf4abade2
liyan <liyan@liyan.local>
2016-07-01 15:54:13 +0800

    更新swift最新语法支持

--- testAutoLayout/CodeAutoLayoutBySwift/ViewController.swift
+++ testAutoLayout/CodeAutoLayoutBySwift/ViewController.swift
@@ -17,25 +17,24 @@ class ViewController: UIViewController {
         
         self.scrollView = UIScrollView()
         self.scrollView.backgroundColor = UIColor.redColor()
-        self.scrollView.setTranslatesAutoresizingMaskIntoConstraints(false)
-        
+        self.scrollView.translatesAutoresizingMaskIntoConstraints = false;
         self.view.addSubview(self.scrollView)
         
-        var constraints = NSLayoutConstraint.constraintsWithVisualFormat("H:|[_scrollView]|", options:NSLayoutFormatOptions(0), metrics: nil, views: ["_scrollView": self.scrollView] )
+        var constraints = NSLayoutConstraint.constraintsWithVisualFormat("H:|[_scrollView]|", options:NSLayoutFormatOptions.DirectionLeadingToTrailing, metrics: nil, views: ["_scrollView": self.scrollView] )
         self.view.addConstraints(constraints)
         
-        constraints = NSLayoutConstraint.constraintsWithVisualFormat("V:|[_scrollView]|", options:NSLayoutFormatOptions(0), metrics: nil, views: ["_scrollView": self.scrollView] )
+        constraints = NSLayoutConstraint.constraintsWithVisualFormat("V:|[_scrollView]|", options:NSLayoutFormatOptions.DirectionLeadingToTrailing, metrics: nil, views: ["_scrollView": self.scrollView] )
         self.view.addConstraints(constraints)
         
-        var contextView = UIView()
+        let contextView = UIView()
         
-        contextView.setTranslatesAutoresizingMaskIntoConstraints(false)
+        contextView.translatesAutoresizingMaskIntoConstraints = false
         contextView.backgroundColor = UIColor.grayColor()
         self.scrollView.addSubview(contextView)
         
-        self.scrollView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[contextView]|", options: NSLayoutFormatOptions(0), metrics: nil, views: ["contextView":contextView]))
+        self.scrollView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[contextView]|", options: NSLayoutFormatOptions.DirectionLeadingToTrailing, metrics: nil, views: ["contextView":contextView]))
         
-        self.scrollView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|[contextView]|", options: NSLayoutFormatOptions(0), metrics: nil, views: ["contextView":contextView]))
+        self.scrollView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|[contextView]|", options: NSLayoutFormatOptions.DirectionLeadingToTrailing, metrics: nil, views: ["contextView":contextView]))
         
         self.scrollView.addConstraint(NSLayoutConstraint(item: contextView, attribute: NSLayoutAttribute.Width, relatedBy: NSLayoutRelation.Equal, toItem: self.scrollView, attribute: NSLayoutAttribute.Width, multiplier: 1, constant: 0))
         
@@ -43,18 +42,18 @@ class ViewController: UIViewController {
         
         var height:CGFloat = 10.0;
         
-        for index in 0...49
+        for _ in 0...49
         {
-            var view = UIView()
+            let view = UIView()
             view.backgroundColor = self.randomColor()
-            view.setTranslatesAutoresizingMaskIntoConstraints(false)
+            view.translatesAutoresizingMaskIntoConstraints = false
             contextView.addSubview(view)
             let attribute :NSLayoutAttribute = (temp != nil) ? NSLayoutAttribute.Bottom : NSLayoutAttribute.Top
             contextView.addConstraint(NSLayoutConstraint(item: view, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: ((temp != nil) ? temp : contextView), attribute:attribute, multiplier: 1, constant: 0))
             
-            contextView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[view]|", options: NSLayoutFormatOptions(0), metrics: nil, views: ["view":view]))
+            contextView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[view]|", options: NSLayoutFormatOptions.DirectionLeadingToTrailing, metrics: nil, views: ["view":view]))
             
-            contextView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:[view(==height)]", options: NSLayoutFormatOptions(0), metrics: ["height":height], views: ["view":view]))
+            contextView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:[view(==height)]", options: NSLayoutFormatOptions.DirectionLeadingToTrailing, metrics: ["height":height], views: ["view":view]))
             temp = view;
             height = height * 1.1
             
@@ -70,9 +69,9 @@ class ViewController: UIViewController {
     
     func randomColor()->UIColor
     {
-        var hue:CGFloat         = CGFloat( arc4random() % 256 ) / 256.0
-        var saturation:CGFloat  = CGFloat( arc4random() % 128 ) / 256.0 + 0.5
-        var brightness:CGFloat  = CGFloat( arc4random() % 128 ) / 256.0 + 0.5
+        let hue:CGFloat         = CGFloat( arc4random() % 256 ) / 256.0
+        let saturation:CGFloat  = CGFloat( arc4random() % 128 ) / 256.0 + 0.5
+        let brightness:CGFloat  = CGFloat( arc4random() % 128 ) / 256.0 + 0.5
         
         return UIColor(hue:hue, saturation:saturation, brightness:brightness, alpha:1.0)
         
